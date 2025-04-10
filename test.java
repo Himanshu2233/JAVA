b609bc3f7a862ab8d83f5b4c3a83d9d97f02daa9
Sandhya Sapkota <sandhyasapkota64@gmail.com>
2025-04-10 10:53:08 +0545

    commit

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test program.");
+        System.out.println("It is designed to demonstrate a simple Java program.");
+        System.out.println("The program prints a few lines of text to the console.");
+        System.out.println("This is the end of the program.");
+    }       
+}
