ced83588686efb2cd36622ea5119b5900b65b86a
Nirjala-Lamichhane <nirjalalamichhane07@gmail.com>
2025-04-10 10:57:25 +0545

    Update test.java

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
         System.out.println("Hello, World!");
         System.out.println("This is a test program.");
         System.out.println("It is designed to demonstrate a simple Java program.");
-        System.out.println("The program prints a few lines of text to the console.");
+        System.out.println("The program prints himanshuuu a few lines of text to the console.");
         System.out.println("This is the end of the program.");
     }       
 }
